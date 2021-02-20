ad12748ca31b270df1df4174227460ad0987a6eb
gsbenevides2 <gsbenevides2@gmail.com>
2021-02-20 23:25:30 +0000

    upgrade expo version and fix eslint issues

--- src/pages/Splash/index.tsx
+++ src/pages/Splash/index.tsx
@@ -1,46 +1,46 @@
+// eslint-disable-next-line no-use-before-define
 import React from 'react'
-import {View, Image} from 'react-native'
+import { View, Image } from 'react-native'
+
 import { useNavigation } from '@react-navigation/native'
-import {AppLoading} from 'expo'
-import {useAssets} from 'expo-asset'
-import {useDatabase} from '../../services/database'
+import AppLoading from 'expo-app-loading'
+import { useAssets } from 'expo-asset'
 
-export default function SplashScreen(){
- const navigation = useNavigation()
- const database = useDatabase()
- const [assets] = useAssets(
-	require('../../../assets/splash.png')
- )
+import { useDatabase } from '../../services/database'
 
- React.useEffect(()=>{
-	if(assets){
-	 (async function(){
-		await database.enableForeignKeys()
-		await database.createTables()
-		navigation.reset({
-		 index:0,
-		 routes:[{name:'tabs'}]
-		})
-	 })()
-	}
- },[assets])
+export default function SplashScreen(): React.ReactNode {
+  const navigation = useNavigation()
+  const database = useDatabase()
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const [assets] = useAssets(require('../../../assets/splash.png'))
 
+  React.useEffect(() => {
+    if (assets) {
+      ;(async function () {
+        await database.enableForeignKeys()
+        await database.createTables()
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'tabs' }]
+        })
+      })()
+    }
+  }, [assets])
 
- if(!assets){
-	return <AppLoading autoHideSplash/>
- }
- else {
-	return (
-	 <View>
-		<Image
-		 style={{
-			width:'100%',
-			 height:'100%'
-		 }}
-		 source={require('../../../assets/splash.png')}
-		 resizeMode='contain'
-		/>
-	 </View>
-	)
- }
+  if (!assets) {
+    return <AppLoading autoHideSplash />
+  } else {
+    return (
+      <View>
+        <Image
+          style={{
+            width: '100%',
+            height: '100%'
+          }}
+          source={require('../../../assets/splash.png')}
+          resizeMode="contain"
+        />
+      </View>
+    )
+  }
 }
